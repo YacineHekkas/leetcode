9a0686de5471b54b9754cc8b6e4daec87488e4ba
YacineHekkas <yac.hakkas@gmail.com>
2024-11-24 15:04:04 +0100

    169. Majority Element problem solved

---
+++ leet-Top Interview 150/169_Majority Element.py
@@ -0,0 +1,29 @@
+from typing import List
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        hash_map = {}
+        for i in nums:
+            print(hash_map)
+            if i in hash_map:
+                print("-------<")
+                hash_map[i] = hash_map.get(i)+1
+                print(hash_map)
+            else:
+                hash_map[i] = 1
+                
+        sorted_by_values = dict(sorted(hash_map.items(), key=lambda item: item[1], reverse=True))
+        print(sorted_by_values)       
+        first_value = next(iter(sorted_by_values.keys()))
+        return first_value
+    
+
+###### check this solution is great unfortunately i didn't think of it thanks to Soumen Maity for his brain
+# class Solution:
+#  def majorityElement(self, nums):
+#   candidate, count = None, 0
+#   for num in nums:
+#    if count == 0:
+#     candidate = num
+#    count += 1 if num == candidate else -1
+#   return candidate
